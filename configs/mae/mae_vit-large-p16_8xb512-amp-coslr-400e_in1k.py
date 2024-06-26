6d06a5f067a36f534d115581c413bb0a95e265c5
Ayush Nangia <55181294+ayushnangia@users.noreply.github.com>
2024-06-26 11:49:05 -0400

    Update mae_vit-large-p16_8xb512-amp-coslr-400e_in1k.py

--- configs/mae/mae_vit-large-p16_8xb512-amp-coslr-400e_in1k.py
+++ configs/mae/mae_vit-large-p16_8xb512-amp-coslr-400e_in1k.py
@@ -12,6 +12,37 @@ train_dataloader = dict(
         with_label=False,
     )
 )
+dataset_type = 'CustomDataset'
+data_root = '/workspace/Dest/'
+data_preprocessor = dict(
+    type='SelfSupDataPreprocessor',
+    mean=[123.675, 116.28, 103.53],
+    std=[58.395, 57.12, 57.375],
+    to_rgb=True)
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='RandomResizedCrop',
+        scale=224,
+        crop_ratio_range=(0.2, 1.0),
+        backend='pillow',
+        interpolation='bicubic'),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PackInputs')
+]
+
+train_dataloader = dict(
+    batch_size=512,
+    num_workers=8,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    collate_fn=dict(type='default_collate'),
+    dataset=dict(
+        type=dataset_type,
+        data_prefix=dict(img='./'),
+        data_root=data_root,
+        pipeline=train_pipeline))
 
 # model settings
 model = dict(
