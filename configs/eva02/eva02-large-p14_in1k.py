326d897347479955bbba5c7d113d3251e4d6d077
Ayush Nangia <55181294+ayushnangia@users.noreply.github.com>
2024-06-26 12:05:02 -0400

    Update eva02-large-p14_in1k.py

--- configs/eva02/eva02-large-p14_in1k.py
+++ configs/eva02/eva02-large-p14_in1k.py
@@ -1,8 +1,48 @@
 _base_ = [
-    '../_base_/datasets/imagenet_bs16_eva_448.py',
+    # '../_base_/datasets/imagenet_bs16_eva_448.py',
     '../_base_/schedules/imagenet_bs2048_AdamW.py',
     '../_base_/default_runtime.py'
 ]
+
+
+# dataset settings
+dataset_type = 'CustomDataset'
+data_root = '/workspace/Dest'
+
+data_preprocessor = dict(
+    type='SelfSupDataPreprocessor',
+    mean=[123.675, 116.28, 103.53],
+    std=[58.395, 57.12, 57.375],
+    to_rgb=True
+)
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='RandomResizedCrop',
+        scale=224,
+        crop_ratio_range=(0.2, 1.0),
+        backend='pillow',
+        interpolation='bicubic'),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PackInputs')
+]
+
+train_dataloader = dict(
+    batch_size=32,
+    num_workers=2,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    collate_fn=dict(type='default_collate'),
+    dataset=dict(
+        type=dataset_type,
+        data_root=data_root,
+        data_prefix=dict(img=''),  # Adjust this if your images are in a subdirectory
+        with_label=False,  # Set to False for unsupervised learning
+        pipeline=train_pipeline
+    )
+)
+
 
 model = dict(
     type='ImageClassifier',
